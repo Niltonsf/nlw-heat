f75dec0607fd7409945d4b72a079a9edbb3f07ee
Niltonsf <nilton.schumacher.filho@gmail.com>
2021-10-19 11:47:29 -0300

    Created messages controller and auth ensure

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controller/AuthenticateUserController';
+import { CreateMessageController } from './controller/CreateMessageController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 export const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle)
+router.post('/authenticate', new AuthenticateUserController().handle);
+
+router.post('/messages', ensureAuthenticated ,new CreateMessageController().handle);
